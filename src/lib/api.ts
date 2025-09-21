e78183b09fef4b6373189bbfbf1c50eb383f9517
srohit310 <rohit.pilakkottil@gmail,com>
2025-09-21 21:55:55 +0530

    changed url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://fastapi-backend-634194827064.asia-south1.run.app';
 
 export interface LoginCredentials {
   email: string;
