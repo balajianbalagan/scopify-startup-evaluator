ae88bd243cfa7111b10d0570fab4c3a894ba505d
Balaji Anbalagan <vijibalaji2003@gmail.com>
2025-09-21 11:38:06 +0530

    fixed flag

--- app/crud/flag.py
+++ app/crud/flag.py
@@ -1,7 +1,7 @@
 # app/crud/flag.py
+from app.schemas.flag import CompanyFlag
 from sqlalchemy.orm import Session
 from app.db.models.company import CompanyInformation
-from app.db.models.flag import CompanyFlag
 
 def get_companies_with_flags(db: Session):
     # Get all companies and their flags
